64bdfcce18a5be9b6ecf2368bccd5f410b85f414
김원태 <rladnjsxo123@naver.com>
2025-02-27 17:25:06 +0900

    108. Meals 데이터와 규격이 불분명한 이미지 출력

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meals Page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favortie recipe and cook it yourself. It&apos;s easy and
+          fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favortie recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
