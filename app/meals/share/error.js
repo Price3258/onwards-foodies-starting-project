3974e7633a8070208ef8f12f7b3227073af2075c
김원태 <rladnjsxo123@naver.com>
2025-02-27 19:39:32 +0900

    126 유효성 추가

---
+++ app/meals/share/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <div>
+        <h1>An Error Occurred</h1>
+        <p>Failed to create meal.</p>
+      </div>
+    </main>
+  );
+}
