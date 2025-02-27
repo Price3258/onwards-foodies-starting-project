b2992ba3a93909b1513fcf58cee75dc1d59db486
김원태 <rladnjsxo123@naver.com>
2025-02-27 18:57:38 +0900

    118. 커스텀 이미지 피커 적용

---
+++ components/meals/image-picker.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { useRef } from "react";
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const imageInput = useRef();
+
+  function handlePickImage() {
+    imageInput.current.click();
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          ref={imageInput}
+          type="file"
+          id={name}
+          name={name}
+          accept="image/png, image/jpeg"
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickImage}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
